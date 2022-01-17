b96a9f5208366c2ba5c4c897809937030a7825d7
lars <lars.scheme@gmail.com>
2022-01-18 00:22:21 +0100

    Format batch scripts

--- install.bat
+++ install.bat
@@ -10,18 +10,18 @@ if not exist "OpenInVim.dll" (
   call build.bat
 )
 
-for /f "tokens=1* usebackq" %%a in (`reg query "HKEY_CURRENT_USER\Software\Borland\Delphi"`) do @(
+for /f "tokens=1* usebackq" %%a in (`reg query "HKEY_CURRENT_USER\Software\Borland\Delphi"`) do (
   reg add "%%a\Experts" /v "Open in Vim" /t REG_SZ /d "%~dp0\OpenInVim.dll"
 )
 
-for /f "tokens=1* usebackq" %%a in (`reg query "HKEY_CURRENT_USER\Software\Borland\BDS"`) do @(
+for /f "tokens=1* usebackq" %%a in (`reg query "HKEY_CURRENT_USER\Software\Borland\BDS"`) do (
   reg add "%%a\Experts" /v "Open in Vim" /t REG_SZ /d "%~dp0\OpenInVim.dll"
 )
 
-for /f "tokens=1* usebackq" %%a in (`reg query "HKEY_CURRENT_USER\Software\CodeGear\BDS"`) do @(
+for /f "tokens=1* usebackq" %%a in (`reg query "HKEY_CURRENT_USER\Software\CodeGear\BDS"`) do (
   reg add "%%a\Experts" /v "Open in Vim" /t REG_SZ /d "%~dp0\OpenInVim.dll"
 )
 
-for /f "tokens=1* usebackq" %%a in (`reg query "HKEY_CURRENT_USER\Software\Embarcadero\BDS"`) do @(
+for /f "tokens=1* usebackq" %%a in (`reg query "HKEY_CURRENT_USER\Software\Embarcadero\BDS"`) do (
   reg add "%%a\Experts" /v "Open in Vim" /t REG_SZ /d "%~dp0\OpenInVim.dll"
 )
--- uninstall.bat
+++ uninstall.bat
@@ -1,17 +1,17 @@
 REM   This will uninstall the plugin from all installed Delphi versions.
 
-for /f "tokens=1* usebackq" %%a in (`reg query "HKEY_CURRENT_USER\Software\Borland\Delphi"`) do @(
+for /f "tokens=1* usebackq" %%a in (`reg query "HKEY_CURRENT_USER\Software\Borland\Delphi"`) do (
   reg delete "%%a\Experts" /v "Open in Vim"
 )
 
-for /f "tokens=1* usebackq" %%a in (`reg query "HKEY_CURRENT_USER\Software\Borland\BDS"`) do @(
+for /f "tokens=1* usebackq" %%a in (`reg query "HKEY_CURRENT_USER\Software\Borland\BDS"`) do (
   reg delete "%%a\Experts" /v "Open in Vim"
 )
 
-for /f "tokens=1* usebackq" %%a in (`reg query "HKEY_CURRENT_USER\Software\CodeGear\BDS"`) do @(
+for /f "tokens=1* usebackq" %%a in (`reg query "HKEY_CURRENT_USER\Software\CodeGear\BDS"`) do (
   reg delete "%%a\Experts" /v "Open in Vim"
 )
 
-for /f "tokens=1* usebackq" %%a in (`reg query "HKEY_CURRENT_USER\Software\Embarcadero\BDS"`) do @(
+for /f "tokens=1* usebackq" %%a in (`reg query "HKEY_CURRENT_USER\Software\Embarcadero\BDS"`) do (
   reg delete "%%a\Experts" /v "Open in Vim"
 )
